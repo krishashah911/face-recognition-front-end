78f5d24e2fd4f16bda81916533106b8cf200972a
krishashah911 <49025746+krishashah911@users.noreply.github.com>
2020-05-19 14:52:23 +0530

    updating localhost

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends React.Component {
       this.state.input)
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://quiet-coast-69894.herokuapp.com/image', {
       method: 'put',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
